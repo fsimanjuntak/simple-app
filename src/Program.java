4948b30febe98e178a290469cec5ab7e00354278
Frans Simanjuntak <franzteve@gmail.com>
2016-09-06 09:20:56 +0200

    bin

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public Program() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
